5434d712cf64c2a882eb77167f8bbd51211ca6df
Amanuel <amanuelbehailu557@gmail.com>
2025-09-27 13:11:00 +0300

    urls working, migrated and successfuly managed to run the server

--- ApiApp/models.py
+++ ApiApp/models.py
@@ -16,13 +16,14 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-class Products(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length = 100)
     description = models.TextField()
     price = models.DecimalField(max_digits = 10, decimal_places = 2)
-    slug = models.SlugField()
+    slug = models.SlugField(unique = True, blank = True, null = True)
     image = models.ImageField(upload_to = "product_img", blank = True, null = True)
-    category = models.ForeignKey(Category, on_delete = models.SET_NULL, blank = True, null = True)
+    featured = models.BooleanField(default = True)
+    category = models.ForeignKey(Category, on_delete = models.SET_NULL, related_name = "products", blank = True, null = True)
 
     def __str__(self):
         return self.name
