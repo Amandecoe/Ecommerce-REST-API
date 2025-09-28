04667d706ecd822d6c7f57da7f88ba5a4516afe8
Amanuel <amanuelbehailu557@gmail.com>
2025-09-28 13:29:46 +0300

    Postgres

--- EcommerceApi/settings.py
+++ EcommerceApi/settings.py
@@ -75,23 +75,28 @@ WSGI_APPLICATION = 'EcommerceApi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-  #  'default': {
-   #     'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-#}
+DB = os.getenv("DB")
 
-DATABASES = {
-    'default' : {
-      'ENGINE' : 'django.db.backends.postgresql',
-      'NAME' : 'railway',
-      'USER' : 'postgres',
-      'PASSWORD' : 'MobUnpEYjnCoWthRZNjvihuVBGHyvdBk',
-      'HOST' : 'tramway.proxy.rlwy.net',
-      'PORT' : '29211',
-    }
-}
+if not DB:
+
+     DATABASES = {
+         'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+         }
+     }
+
+else:
+    DATABASES = {
+        'default' : {
+            'ENGINE' : 'django.db.backends.postgresql',
+            'NAME' : 'railway',
+            'USER' : 'postgres',
+            'PASSWORD' : 'MobUnpEYjnCoWthRZNjvihuVBGHyvdBk',
+            'HOST' : 'tramway.proxy.rlwy.net',
+            'PORT' : '29211',
+        }
+     }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
