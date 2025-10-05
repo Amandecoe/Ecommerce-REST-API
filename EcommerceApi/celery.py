4a5be88a6e43ce84e721f6fef8d9d88f122514a6
Amanuel <amanuelbehailu557@gmail.com>
2025-10-05 11:20:54 +0300

    celery beat weekly newsletter configured in celery

--- EcommerceApi/celery.py
+++ EcommerceApi/celery.py
@@ -1,8 +1,17 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EcommerceApi.settings")
 
 app = Celery ("EcommerceApi")
 app.config_from_object("django.conf:settings", namespace = "CELERY")
 app.autodiscover_tasks()
+
+#scheduled weekly newsletter
+app.conf.beat_schedule = {
+    "send-weekly-newsletter" : {
+        "task" : "ApiApp.tasks.send_weekly_newsletter",
+        "schedule" : crontab (hour = 9, minute = 0, day_of_week = "mon") #sends the news letter every monday
+    }
+}
